12cb9b0cbc9f67463c95e825be09fe47a9202a1d
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-10-18 02:55:05 +0600

    fix error for preview image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,40 @@
-import localFont from "next/font/local";
+// app/layout.js
+
 import "./glob.css";
-import Head from "next/head"; 
 
 export const metadata = {
   title: "Link Sharing App",
   description: "Share Your Link Simple Way!",
   icons: {
-    icon: '/images/logo-devlinks-small.svg'
-  }
+    icon: '/images/logo-devlinks-small.svg',
+  },
+  openGraph: {
+    title: "Link Sharing App",
+    description: "Share Your Link Simple Way!",
+    url: "https://ismail-link-sharing-app.vercel.app/", // Replace with your actual URL
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/thypgk1nmxm4modj1wdl.jpg",
+        width: 700,
+        height: 700,
+        alt: "Link Sharing App Preview Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Link Sharing App",
+    description: "Share Your Link Simple Way!",
+    images: [
+      "https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/thypgk1nmxm4modj1wdl.jpg",
+    ],
+  },
 };
 
-export default function RootLayout({ children }) { // Accept previewImage as a prop
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        
-        <meta property="og:image" content="https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/thypgk1nmxm4modj1wdl.jpg" /> 
-      </Head>
       <body className="bg-[#FAFAFA]">{children}</body>
     </html>
   );
