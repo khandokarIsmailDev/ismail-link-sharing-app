5d747c3227023878db9c3613d4ca23d67f250983
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-10-18 02:31:02 +0600

    add preview image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
 import localFont from "next/font/local";
 import "./glob.css";
+import Head from "next/head"; 
 
 export const metadata = {
   title: "Link Sharing App",
   description: "Share Your Link Simple Way!",
-  icons:{
-    icon:'/images/logo-devlinks-small.svg'
+  icons: {
+    icon: '/images/logo-devlinks-small.svg'
   }
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }) { // Accept previewImage as a prop
   return (
     <html lang="en">
+      <Head>
+        
+        <meta property="og:image" content="/images/man.png" /> 
+      </Head>
       <body className="bg-[#FAFAFA]">{children}</body>
     </html>
   );
